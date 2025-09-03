3deb4c1a66ae7a11e551602b0e8415eee548056f
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 02:33:58 -0300

    Iniciando a Fundação do Projeto de Monitoramento Escolar.

---
+++ src/main/java/com/projeto/monitoramento/MonitoramentoEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.monitoramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/monitoramento/MonitoramentoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
