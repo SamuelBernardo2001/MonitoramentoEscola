9e8e48074f611e3f6c6e37967e8a826b8d417d1c
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:42:12 -0300

    Alterações: Adicionado relacionamento ManyToMany com Aluno para associar disciplinas aos alunos.

--- src/main/java/com/projeto/monitoramento/instituto/disciplina/DisciplinaModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/disciplina/DisciplinaModel.java
@@ -2,8 +2,12 @@ package com.projeto.monitoramento.instituto.disciplina;
 
 import com.projeto.monitoramento.instituto.professor.ProfessorModel;
 import com.projeto.monitoramento.instituto.turma.TurmaModel;
+import com.projeto.monitoramento.instituto.aluno.AlunoModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,22 +15,29 @@ import lombok.*;
 @Setter
 @Builder
 @Entity
-@Table (name = "disciplina")
+@Table(name = "disciplina")
 public class DisciplinaModel {
 
+    // ID da disciplina (PK)
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Nome da disciplina (obrigatório)
+    @NotNull
     private String nome;
 
-    // Disciplina -> Professor (N:1)
+    // Disciplina -> Professor (N:1) : Cada disciplina é lecionada por um professor
     @ManyToOne
     @JoinColumn(name = "professor_id")
     private ProfessorModel professor;
 
-    // Disciplina -> Turma (N:1)
+    // Disciplina -> Turma (N:1) : Cada disciplina pertence a uma turma
     @ManyToOne
     @JoinColumn(name = "turma_id")
     private TurmaModel turma;
+
+    // Disciplina -> Alunos (N:M) : Uma disciplina pode ter vários alunos
+    @ManyToMany(mappedBy = "disciplinas")
+    private Set<AlunoModel> alunos;
 }
