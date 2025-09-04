e2731ccd801c38bb72b74a5834016d9efebea790
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:46:14 -0300

    Alterações: Adicionados @NotNull em nome, telefone e relacao. Comentários detalhados adicionados.

--- src/main/java/com/projeto/monitoramento/responsavel/ResponsavelModel.java
+++ src/main/java/com/projeto/monitoramento/responsavel/ResponsavelModel.java
@@ -1,8 +1,8 @@
 package com.projeto.monitoramento.responsavel;
 
 import com.projeto.monitoramento.instituto.aluno.AlunoModel;
-import com.projeto.monitoramento.instituto.professor.ProfessorModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -16,16 +16,27 @@ import java.util.List;
 @Table(name = "responsavel")
 public class ResponsavelModel {
 
+    // ID do responsável (PK)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Nome do responsável (obrigatório)
+    @NotNull
     private String nome;
-    private String telefone;
-    private String email;
-    private String relacao; // Ex: Pai, Mãe, Tio, Avó etc.
 
-    // Relacionamento: Responsável -> Aluno (1:N)
+    // Telefone de contato (obrigatório)
+    @NotNull
+    private String telefone;
+
+    // E-mail do responsável
+    private String email;
+
+    // Relação com o aluno (Pai, Mãe, Tio, Avó, etc.) (obrigatório)
+    @NotNull
+    private String relacao;
+
+    // Responsável -> Alunos (1:N) : Um responsável pode ter vários alunos
     @OneToMany(mappedBy = "responsavel", cascade = CascadeType.ALL)
     private List<AlunoModel> alunos;
 }
