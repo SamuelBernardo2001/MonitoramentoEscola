a2010f2d3ddcfae82fb81dc77a430314d95301fd
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:05:59 -0300

    adicionando a variavel serie e adicionando outros relacionamentos como a frequencia e notas e relatorios.

--- src/main/java/com/projeto/monitoramento/instituto/aluno/AlunoModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/aluno/AlunoModel.java
@@ -1,9 +1,17 @@
 package com.projeto.monitoramento.instituto.aluno;
 
+import com.projeto.monitoramento.instituto.dadosAcademicos.frequencia.FrequenciaModel;
+import com.projeto.monitoramento.instituto.dadosAcademicos.notas.NotaModel;
+import com.projeto.monitoramento.instituto.escola.EscolaModel;
+import com.projeto.monitoramento.instituto.professor.ProfessorModel;
 import com.projeto.monitoramento.instituto.turma.TurmaModel;
 import com.projeto.monitoramento.responsavel.ResponsavelModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,20 +26,39 @@ public class AlunoModel {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 3, max = 100)
     private String nome;
     private int idade;
+
+    @NotNull
     private String matricula;
     private int comportamento;
     private int desempenho;
 
-    // Aluno -> Turma (N:1)
+    private String serie;
+
+    // Relacionamentos
     @ManyToOne
     @JoinColumn(name = "turma_id")
     private TurmaModel turma;
 
-    // Aluno -> Responsável (N:1)
     @ManyToOne
     @JoinColumn(name = "responsavel_id")
     private ResponsavelModel responsavel;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private ProfessorModel professor;
+
+    @ManyToOne
+    @JoinColumn(name = "escola_id")
+    private EscolaModel escola;
+
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL)
+    private List<NotaModel> notas;
+
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL)
+    private List<FrequenciaModel> frequencias;
 }
 
