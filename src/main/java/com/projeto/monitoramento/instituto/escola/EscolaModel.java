4d2ece069a663818207a68d18015aab287916eac
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:42:58 -0300

    Alterações: Adicionados @NotNull em nome, endereco, contato e diretor para integridade.

--- src/main/java/com/projeto/monitoramento/instituto/escola/EscolaModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/escola/EscolaModel.java
@@ -3,6 +3,7 @@ package com.projeto.monitoramento.instituto.escola;
 import com.projeto.monitoramento.instituto.aluno.AlunoModel;
 import com.projeto.monitoramento.instituto.professor.ProfessorModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -13,23 +14,35 @@ import java.util.List;
 @Setter
 @Builder
 @Entity
-@Table (name = "escola")
+@Table(name = "escola")
 public class EscolaModel {
 
+    // ID da escola (PK)
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Nome da escola (obrigatório)
+    @NotNull
     private String nome;
+
+    // Endereço da escola (obrigatório)
+    @NotNull
     private String endereco;
+
+    // Contato da escola (obrigatório)
+    @NotNull
     private String contato;
+
+    // Diretor responsável
+    @NotNull
     private String diretor;
 
+    // Escola -> Alunos (1:N) : Uma escola possui vários alunos
     @OneToMany(mappedBy = "escola", cascade = CascadeType.ALL)
     private List<AlunoModel> alunos;
 
+    // Escola -> Professores (1:N) : Uma escola possui vários professores
     @OneToMany(mappedBy = "escola", cascade = CascadeType.ALL)
     private List<ProfessorModel> professores;
-
-
 }
