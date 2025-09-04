b7f518108cf2928bd3b726909b1ba69fcf4f478b
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:39:29 -0300

    Alterações: Adicionado @NotNull para data e presenca para garantir integridade.

--- src/main/java/com/projeto/monitoramento/instituto/dadosAcademicos/frequencia/FrequenciaModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/dadosAcademicos/frequencia/FrequenciaModel.java
@@ -2,6 +2,7 @@ package com.projeto.monitoramento.instituto.dadosAcademicos.frequencia;
 
 import com.projeto.monitoramento.instituto.aluno.AlunoModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -15,17 +16,21 @@ import java.time.LocalDate;
 @Table(name = "frequencia")
 public class FrequenciaModel {
 
+    // ID da frequência (PK)
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Data da frequência (obrigatória)
+    @NotNull
     private LocalDate data;
 
+    // Presença ou ausência do aluno
+    @NotNull
     private Boolean presenca;
 
-    // Frequência -> Aluno (N:1)
+    // Frequência -> Aluno (N:1) : Cada registro de frequência pertence a um aluno
     @ManyToOne
     @JoinColumn(name = "aluno_id")
     private AlunoModel aluno;
-
 }
