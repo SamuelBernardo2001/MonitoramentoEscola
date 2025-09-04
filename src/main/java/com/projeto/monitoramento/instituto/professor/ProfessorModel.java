05a52c7b507a0ed6f4884a1e81372909632dde09
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:44:45 -0300

    Alterações: Adicionados @NotNull em nome, email e telefone. Comentários detalhados adicionados.

--- src/main/java/com/projeto/monitoramento/instituto/professor/ProfessorModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/professor/ProfessorModel.java
@@ -6,6 +6,7 @@ import com.projeto.monitoramento.instituto.disciplina.DisciplinaModel;
 import com.projeto.monitoramento.instituto.escola.EscolaModel;
 import com.projeto.monitoramento.instituto.turma.TurmaModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -16,35 +17,47 @@ import java.util.List;
 @Setter
 @Builder
 @Entity
-@Table (name = "professor")
+@Table(name = "professor")
 public class ProfessorModel {
 
+    // ID do professor (PK)
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Nome do professor (obrigatório)
+    @NotNull
     private String nome;
+
+    // E-mail do professor (obrigatório)
+    @NotNull
     private String email;
+
+    // Telefone de contato
+    @NotNull
     private String telefone;
+
+    // Especialidade do professor
     private String especialidade;
 
-    // Professor -> Turmas (1:N)
+    // Professor -> Turmas (1:N) : Um professor pode lecionar várias turmas
     @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL)
     private List<TurmaModel> turmas;
 
-    // Professor -> Disciplinas (1:N)
+    // Professor -> Disciplinas (1:N) : Um professor pode lecionar várias disciplinas
     @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL)
     private List<DisciplinaModel> disciplinas;
 
+    // Professor -> Alunos (1:N) : Um professor pode ser tutor de vários alunos
     @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL)
     private List<AlunoModel> alunos;
 
+    // Professor -> Escola (N:1) : Cada professor pertence a uma escola
     @ManyToOne
     @JoinColumn(name = "escola_id")
     private EscolaModel escola;
 
-    //Cada professor pode criar muitos relatórios sobre diferentes alunos:
+    // Professor -> Relatórios (1:N) : Um professor pode criar vários relatórios de alunos
     @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL)
     private List<RelatorioAlunoModel> relatorios;
-
 }
