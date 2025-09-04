2e8d96f81f3e347c31a0fe9782525c8b905ce5ce
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:40:25 -0300

    Alterações: Adicionado @NotNull em valor e bimestre para integridade.

--- src/main/java/com/projeto/monitoramento/instituto/dadosAcademicos/notas/NotaModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/dadosAcademicos/notas/NotaModel.java
@@ -3,6 +3,7 @@ package com.projeto.monitoramento.instituto.dadosAcademicos.notas;
 import com.projeto.monitoramento.instituto.aluno.AlunoModel;
 import com.projeto.monitoramento.instituto.disciplina.DisciplinaModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -14,22 +15,26 @@ import lombok.*;
 @Table(name = "notas")
 public class NotaModel {
 
+    // ID da nota (PK)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Valor da nota (obrigatório)
+    @NotNull
     private Double valor;
 
+    // Bimestre referente à nota (obrigatório)
+    @NotNull
     private Integer bimestre;
 
-    // Nota -> Disciplina (N:1)
+    // Nota -> Disciplina (N:1) : Cada nota pertence a uma disciplina
     @ManyToOne
     @JoinColumn(name = "disciplina_id")
     private DisciplinaModel disciplina;
 
-    // Nota -> Aluno (N:1)
+    // Nota -> Aluno (N:1) : Cada nota pertence a um aluno
     @ManyToOne
     @JoinColumn(name = "aluno_id")
     private AlunoModel aluno;
-
 }
