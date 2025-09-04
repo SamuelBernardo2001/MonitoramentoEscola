89197ba9081139df5edb297d6861b2ebc6d1dbf5
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:41:21 -0300

    Alterações: Adicionado @NotNull em observacoes para garantir registro do relatório.

--- src/main/java/com/projeto/monitoramento/instituto/dadosAcademicos/relatorio/RelatorioAlunoModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/dadosAcademicos/relatorio/RelatorioAlunoModel.java
@@ -3,6 +3,7 @@ package com.projeto.monitoramento.instituto.dadosAcademicos.relatorio;
 import com.projeto.monitoramento.instituto.aluno.AlunoModel;
 import com.projeto.monitoramento.instituto.professor.ProfessorModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -14,22 +15,25 @@ import lombok.*;
 @Table(name = "relatorio_aluno")
 public class RelatorioAlunoModel {
 
+    // ID do relatório (PK)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Observações do professor sobre o aluno
+    @NotNull
     private String observacoes;
 
+    // Dificuldades do aluno relatadas pelo professor
     private String dificuldades;
 
-    // Relatório -> Aluno (N:1)
+    // Relatório -> Aluno (N:1) : Cada relatório pertence a um aluno
     @ManyToOne
     @JoinColumn(name = "aluno_id")
     private AlunoModel aluno;
 
-    // Relatório -> Professor (N:1)
+    // Relatório -> Professor (N:1) : Cada relatório é feito por um professor
     @ManyToOne
     @JoinColumn(name = "professor_id")
     private ProfessorModel professor;
-
 }
