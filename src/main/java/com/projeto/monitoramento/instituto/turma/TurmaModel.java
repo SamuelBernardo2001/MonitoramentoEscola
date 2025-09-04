2a6d22260b229fcf33b250babe04e8b7a12fe364
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-03 21:45:26 -0300

    Alterações: Adicionados @NotNull em nome, ano e turno. Comentários detalhados adicionados.

--- src/main/java/com/projeto/monitoramento/instituto/turma/TurmaModel.java
+++ src/main/java/com/projeto/monitoramento/instituto/turma/TurmaModel.java
@@ -4,6 +4,7 @@ import com.projeto.monitoramento.instituto.aluno.AlunoModel;
 import com.projeto.monitoramento.instituto.disciplina.DisciplinaModel;
 import com.projeto.monitoramento.instituto.professor.ProfessorModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -14,27 +15,36 @@ import java.util.List;
 @Setter
 @Builder
 @Entity
-@Table (name = "turma")
+@Table(name = "turma")
 public class TurmaModel {
 
+    // ID da turma (PK)
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Nome da turma (obrigatório)
+    @NotNull
     private String nome;
+
+    // Ano da turma (obrigatório)
+    @NotNull
     private String ano;
+
+    // Turno da turma (obrigatório)
+    @NotNull
     private String turno;
 
-    // Turma -> Professor (N:1)
+    // Turma -> Professor (N:1) : Cada turma tem um professor responsável
     @ManyToOne
     @JoinColumn(name = "professor_id")
     private ProfessorModel professor;
 
-    // Turma -> Alunos (1:N)
+    // Turma -> Alunos (1:N) : Uma turma possui vários alunos
     @OneToMany(mappedBy = "turma", cascade = CascadeType.ALL)
     private List<AlunoModel> alunos;
 
-    // Turma -> Disciplinas (1:N)
+    // Turma -> Disciplinas (1:N) : Uma turma possui várias disciplinas
     @OneToMany(mappedBy = "turma", cascade = CascadeType.ALL)
     private List<DisciplinaModel> disciplinas;
 }
